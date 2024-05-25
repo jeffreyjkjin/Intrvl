d6e2bf5948aa71e3389d0f0e0e81c806bd494803
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-05-24 20:40:55 -0700

    Created PageHeader component
    -Implemented Figma design for header

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-blue-500 p-6 font-roboto font-black italic text-5xl text-center text-white">
+        <RouterLink to="/">
+            INTRVL
+        </RouterLink>
+    </div>
+</template>
