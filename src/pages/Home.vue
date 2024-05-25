69e26287e91faca7c7af19ac0d35b46a0fab247a
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-05-24 22:18:53 -0700

    Updated Home page
    -Implemented logic for loading timers from db and creating new timers

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,56 @@
+<script setup lang="ts">
+import { onBeforeMount, ref } from 'vue'
+import { Router, useRouter } from 'vue-router'
+
+import PageHeader from '../components/PageHeader.vue'
+import { Timer, timers } from '../db'
+
+const router: Router = useRouter();
+const timerArr = ref<Timer[]>([]);
+
+const getAllTimers = async (): Promise<Timer[]> => {
+    return timers.timers.toArray();
+}
+
+const createTimer = () => {
+    const datetime: number = Date.now();
+
+    const t: Timer = {
+        datetime: datetime,
+        name: `New Timer`,
+        rounds: 0,
+        intervals: []         
+    }
+
+    // add timer to db and redirect to timer details page
+    timers.timers.add(t)
+        .then(() => router.push(`/timer/${datetime}`))
+        .catch((err: any) => console.log(err));
+}
+
+// grab all timers from db
+onBeforeMount( () => {
+    getAllTimers()
+        .then((res: Timer[]) => timerArr.value = res)
+        .catch((err: any) => console.error(err));
+});
+</script>
+
 <template>
-    Home Page
+    <PageHeader />
+
+    <div v-if="timerArr.length">
+        <div v-for="timer in timerArr" :key="timer.datetime">
+            <RouterLink :to="'/timer/' + timer.datetime">
+                {{ timer.name }}
+            </RouterLink>
+        </div>
+    </div>
+    <div v-else>
+        No timers found
+    </div>
+
+    <button @click="createTimer">
+        Create New Timer
+    </button>
 </template>
