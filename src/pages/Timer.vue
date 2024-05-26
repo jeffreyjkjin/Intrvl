729446c0f69ced1dbcb04c5ae369c10a26ca87a1
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-05-25 22:38:04 -0700

    Updater Timer page
    -Implemented logic to fetch timer from db using datetime url param
    -Display information about timer

--- src/pages/Timer.vue
+++ src/pages/Timer.vue
@@ -1,3 +1,72 @@
+<script setup lang="ts">
+import { onBeforeMount, ref } from 'vue'
+import { RouteLocationNormalizedLoaded, Router, useRoute, useRouter } from 'vue-router'
+
+import PageHeader from '../components/PageHeader.vue'
+import { Timer, timers } from '../db'
+
+const route: RouteLocationNormalizedLoaded = useRoute();
+const router: Router = useRouter();
+const timer = ref<Timer>();
+
+// gets timer with datetime param
+const getTimer = async (): Promise<Timer> => {
+    const t: Timer | undefined = await timers.timers.get(Number(route.params.datetime));
+
+    if (t === undefined) {
+        throw Error();
+    }
+
+    return t;
+}
+
+// grab timer from db with datetime param
+onBeforeMount(() => {
+    getTimer()
+        .then((res: Timer | undefined) => timer.value = res)
+        .catch(() => router.push('/error'));
+});
+
+</script>
+
 <template>
-    Timer Details
+    <PageHeader />
+
+    <div v-if="timer !== undefined">
+        <div>
+            {{ timer.name}}
+        </div>
+        <div>
+            Total Time:
+        </div>
+        <div>
+            Rounds: {{ timer.rounds }}
+        </div>
+        <div>
+            Intervals:
+            <div v-for="interval in timer.intervals">
+                <div>
+                    {{ interval.name }}
+                </div>
+                <div>
+                    {{ interval.colour }}
+                </div>
+                <div>
+                    {{ interval.length }}
+                </div>
+                <div>
+                    {{ interval.warning }}
+                </div>
+                <div>
+                    {{ interval.warningTime }}
+                </div>
+                <div>
+                    {{ interval.sound }}
+                </div>
+                <div>
+                    {{ interval.repeat }}
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
