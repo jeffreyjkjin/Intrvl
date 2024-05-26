6fe0a8d5fcf06bb6fe229e3337809e556ca35828
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-05-26 15:44:06 -0700

    Updated Timer page
    -Added button to add a new interval to the timer

--- src/pages/Timer.vue
+++ src/pages/Timer.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { onBeforeMount, ref } from 'vue'
+import { onBeforeMount, ref, toRaw } from 'vue'
 import { RouteLocationNormalizedLoaded, Router, useRoute, useRouter } from 'vue-router'
 
 import PageHeader from '../components/PageHeader.vue'
-import { Timer, timers } from '../db'
+import { Interval, Timer, timers } from '../db'
 
 const route: RouteLocationNormalizedLoaded = useRoute();
 const router: Router = useRouter();
@@ -18,6 +18,30 @@ const getTimer = async (): Promise<Timer> => {
     }
 
     return t;
+}
+
+// creates a new empty interval and adds it to intervals array of timer in db 
+const createInterval = async () => {
+    if (timer.value) {
+        const i: Interval = {
+            name: 'New Interval',
+            colour: '',
+            length: 0,
+            warning: '',
+            warningTime: 0,
+            sound: '',
+            repeat: false
+        }
+
+        // insert interval into timer in db
+        timer.value.intervals.push(i);
+
+        await timers.timers.update(
+            Number(route.params.datetime),
+            { intervals: toRaw(timer.value.intervals) }
+        )
+            .catch((err: any) => console.error(err));
+    }
 }
 
 // grab timer from db with datetime param
@@ -69,4 +93,8 @@ onBeforeMount(() => {
             </div>
         </div>
     </div>
+
+    <button @click="createInterval">
+        Add New Interval
+    </button>
 </template>
