b23dfe343ed2fdc482c5de9b27a4f4a99d44482f
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-06-07 21:01:26 -0700

    Updated Start page
    -Added TimerProgress component
    -Replaced setInterval with setTimeout
    -Moved interval logic from startTimer to runTimer function
    -Added references to track milliseconds
    -Added timer drift adjustment to runTimer

--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -2,27 +2,33 @@
 import { onBeforeMount, ref, watch } from 'vue'
 import { RouteLocationNormalizedLoaded, Router, useRoute, useRouter } from 'vue-router'
 
+import TimerProgress from '../components/TimerProgress.vue'
 import { Interval, Timer } from '../utilities/db'
 import getTimer from '../utilities/getTimer'
 import playSound from '../utilities/playSound'
 
 const route: RouteLocationNormalizedLoaded = useRoute();
 const router: Router = useRouter();
+
 const timer = ref<Timer>();
 
-let intervalID: number;
-
+// constants for timer
 const numIntervals = ref<number>(0);
 const totalRounds = ref<number>(0);
 
+// tracking time/rounds
 const currentRound = ref<number>(0);
+const expectedTime = ref<number>(0);
+const ms = ref<number>(0);
 const remainingTime = ref<number>(0);
 const roundTime = ref<number>(0);
 
+// timer booleans
 const timerPause = ref<boolean>(false);
 const timerStarted = ref<boolean>(false);
 
 const timerColour = ref<string>('');
+
 
 // calculates total time of timer based on number of rounds remaining
 const getTotalTime = (rounds: number): number => {
@@ -39,63 +45,86 @@ const getTotalTime = (rounds: number): number => {
     return time;
 }
 
+const runInterval = () => {
+    // if timer ended
+    if (!timerStarted.value) return;
+
+    // adjust timer for drift
+    const delta: number = Date.now() - expectedTime.value;
+
+    if (delta > 10) expectedTime.value += 10 * Math.floor(delta / 10);
+    else expectedTime.value += 10;
+    
+    setTimeout(runInterval, Math.max(0, 10 - delta));
+    
+    // stop timer countdown if paused
+    if (timerPause.value) return;
+
+    // update milliseconds based on drift
+    if (delta > 10) ms.value += 10 * Math.floor(delta / 10);
+    else ms.value += 10;
+
+    // wait until at least 1 second has passed before incrementing timers
+    if (ms.value < 1000) return;
+    ms.value = 0;
+
+    roundTime.value--;
+    remainingTime.value--;
+
+    const intervals: Interval[] = (timer.value as Timer).intervals;
+    const intervalIndex: number = currentRound.value % numIntervals.value;
+
+    // play warning sound
+    if (roundTime.value === intervals[intervalIndex].warningTime) {
+        playSound(intervals[intervalIndex].warning);
+    } 
+
+    // when timer finishes
+    if (!remainingTime.value) {
+        playSound(intervals[intervalIndex].sound);
+
+        timerStarted.value = false;
+
+        return;
+    };
+            
+    // when interval finishes
+    if (!roundTime.value) {
+        playSound(intervals[intervalIndex].sound);
+
+        // find next interval with repeat on
+        do {
+            currentRound.value++;
+        } while (currentRound.value >= numIntervals.value && !intervals[currentRound.value % numIntervals.value].repeat);
+
+        roundTime.value = intervals[currentRound.value % numIntervals.value].length;
+    }
+}
+
 // sets an interval for the timer and clears it when the timer is done
 const startTimer = () => {
-    const intervals: Interval[] = (timer.value as Timer).intervals;
-
     // set up timer
     currentRound.value = 0;
+    ms.value = 0;
     remainingTime.value = getTotalTime(totalRounds.value);
-    roundTime.value = intervals[0].length;
+    roundTime.value = (timer.value as Timer).intervals[0].length;
 
     timerStarted.value = true;
 
-    intervalID = setInterval(() => {
-        // stop timer countdown if paused
-        if (timerPause.value) return;
+    expectedTime.value = Date.now() + 10;
 
-        roundTime.value--;
-        remainingTime.value--;
-        
-        const intervalIndex: number = currentRound.value % numIntervals.value;
-
-        if (roundTime.value === intervals[intervalIndex].warningTime) 
-            playSound(intervals[intervalIndex].warning);
-
-        // when timer finishes
-        if (!remainingTime.value) {
-            playSound(intervals[intervalIndex].sound);
-
-            clearInterval(intervalID);
-
-            timerStarted.value = false;
-
-            return;
-        };
-            
-        // when interval finishes
-        if (!roundTime.value) {
-            playSound(intervals[intervalIndex].sound);
-
-            // find next interval with repeat on
-            do {
-                currentRound.value++;
-            } while (currentRound.value >= numIntervals.value && !intervals[currentRound.value % numIntervals.value].repeat);
-
-            roundTime.value = intervals[currentRound.value % numIntervals.value].length;
-        } 
-    }, 1000);
+    setTimeout(runInterval, 10);
 }
 
 // rewinds current round by one
 const rewind = () => {
+    // if current round is the first one
     if (currentRound.value === 0) return;
 
     const intervals: Interval[] = (timer.value as Timer).intervals;
     playSound(intervals[currentRound.value % numIntervals.value].sound);
 
     // find last interval with repeat on
-    // while (!intervals[--currentRound.value % numIntervals.value].repeat && currentRound.value >= totalRounds.value);
     do {
         currentRound.value--;
     } while (currentRound.value >= numIntervals.value && !intervals[currentRound.value % numIntervals.value].repeat);  
@@ -106,6 +135,7 @@ const rewind = () => {
 
 // skips current round
 const forward = () => {
+    // if current round is the last one
     if (currentRound.value === totalRounds.value - 1) return;
 
     const intervals: Interval[] = (timer.value as Timer).intervals;
@@ -130,8 +160,9 @@ onBeforeMount(() => {
             totalRounds.value = (timer.value as Timer).rounds * numIntervals.value;
         })
         .then(() => {
-            // initialize times
+            // initialize timer values
             currentRound.value = 0;
+            ms.value = 0;
             remainingTime.value = getTotalTime(totalRounds.value);
             roundTime.value = (timer.value as Timer).intervals[0].length;
 
@@ -180,9 +211,7 @@ watch(currentRound, () => {
                 </button>
             </div>
         </div>
-        <div v-if="numIntervals">
-            {{ timer.intervals[currentRound % numIntervals].name }}: {{ roundTime }}
-        </div>
+        <TimerProgress :seconds="roundTime" :ms="ms" :interval="timer.intervals[currentRound % numIntervals]" />
         <div>
             Remaining Time: {{ remainingTime }}
         </div>
