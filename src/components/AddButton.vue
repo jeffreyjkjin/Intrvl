f0d12ae56ebc1351276d5c1849f2b781bb840945
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-06-13 21:55:00 -0700

    Created AddButton component
    -Implemented figma design for button to add new interval/timer
    -Added svg for plus icon

---
+++ src/components/AddButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Plus from '../icons/plus.svg'
+
+</script>
+
+<template>
+    <button class="fixed bottom-4 right-4 p-3 bg-blue-500 rounded-full">
+        <Plus class="w-12 h-12 fill-white" />
+    </button>
+</template>
