21d70e9fbccf928bf9df3b525a2557ca316d6be0
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-06-22 20:19:26 -0700

    Updated colour
    -Replaced blue-500 with blue-600 for PageHeader, AddButton, and favicon

--- src/components/AddButton.vue
+++ src/components/AddButton.vue
@@ -4,7 +4,7 @@ import Plus from '../icons/plus.svg'
 </script>
 
 <template>
-    <button class="fixed bottom-4 right-4 p-3 bg-blue-500 rounded-full">
+    <button class="fixed bottom-4 right-4 p-3 bg-blue-600 rounded-full">
         <Plus class="w-12 h-12 fill-white" />
     </button>
 </template>
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-blue-500 p-6 font-roboto font-black italic text-5xl text-center text-white">
+    <div class="bg-blue-600 p-6 font-roboto font-black italic text-5xl text-center text-white">
         <RouterLink to="/">
             INTRVL
         </RouterLink>
