8c4d9e52d0d87d98a4199cbcf811f0e6d867e5c6
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-06-22 22:47:14 -0700

    Updated Error page
    -Implemented figma design

--- src/pages/Error.vue
+++ src/pages/Error.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import PageHeader from '../components/PageHeader.vue'
+</script>
+
 <template>
-    Error Page
+    <PageHeader />
+    <div class="flex flex-col font-roboto text-center m-4 gap-6 pt-36">
+        <div class="font-bold text-9xl">
+            Oops!
+        </div>
+        <div class="text-xl">
+            The page you were looking for could not be found.
+            <RouterLink class="text-blue-400 inline" to="/home">
+                Click here to go back to the home page.
+            </RouterLink>
+        </div>
+    </div>
 </template>
