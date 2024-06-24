d9b47eed5a8c88d2889641e58603249db8eef4b9
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-06-23 22:20:03 -0700

    Updated PageHeader component
    -Updated router link to go to home page

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-blue-600 p-6 font-roboto font-black italic text-5xl text-center text-white">
-        <RouterLink to="/">
+        <RouterLink to="/home">
             INTRVL
         </RouterLink>
     </div>
