62bd5dee9af47022ab75c1903e95d61f37be84a5
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-06-24 22:58:07 -0700

    Updated PageHeader component
    -Updated css to be responsive for tablets/desktop

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-blue-600 p-6 font-roboto font-black italic text-5xl text-center text-white">
+    <div class="bg-blue-600 p-6 font-roboto font-black italic text-center text-white text-5xl md:text-6xl">
         <RouterLink to="/home">
             INTRVL
         </RouterLink>
