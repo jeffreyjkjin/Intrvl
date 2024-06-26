d4fb20650b7d8f4e741b1320af9816ff5844babb
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-06-25 20:01:46 -0700

    Updated Landing page
    -Replace css for min screen with min dvh

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="font-roboto text-white min-w-screen min-h-screen bg-gradient-to-r from-blue-950 via-blue-900 to-blue-800">
+    <div class="font-roboto text-white min-w-dvh min-h-dvh bg-gradient-to-r from-blue-950 via-blue-900 to-blue-800">
         <div class="flex flex-col gap-12 font-black absolute top-1/2 -translate-y-1/2 left-2 md:left-1/2 md:-translate-x-1/2 md:place-items-center">
             <div class="md:text-center">
                 <div class="italic leading-[0.8] text-[104px] md:text-9xl md:leading-[0.7]">
