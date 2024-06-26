b304aa62a687b9543d1afbe4517294e20cc915f5
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-06-25 19:14:43 -0700

    Updated Landing page
    -Updated css to be responsive for desktops/tablets

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="font-roboto text-white min-w-screen min-h-screen bg-gradient-to-r from-blue-950 via-blue-900 to-blue-800">
-        <div class="flex flex-col gap-12 font-black absolute top-1/2 -translate-y-1/2 left-2">
-            <div>
-                <div class="italic text-[104px] leading-[0.8]">
+        <div class="flex flex-col gap-12 font-black absolute top-1/2 -translate-y-1/2 left-2 md:left-1/2 md:-translate-x-1/2 md:place-items-center">
+            <div class="md:text-center">
+                <div class="italic leading-[0.8] text-[104px] md:text-9xl md:leading-[0.7]">
                     INTRVL
                 </div>
-                <div class="text-[17px]">
+                <div class="text-[17px] md:text-[21px]">
                     Just a Simple, Customizable Interval Timer.
                 </div>
             </div>
