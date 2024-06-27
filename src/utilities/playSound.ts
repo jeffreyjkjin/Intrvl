79143d8598d1fd3b2a47ffd16544bae92ddb1e92
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-06-27 01:01:26 -0700

    Updated playSounds
    -Updated function to work with Howler

--- src/utilities/playSound.ts
+++ src/utilities/playSound.ts
@@ -2,8 +2,8 @@ import { sounds } from "../utilities/sounds"
 
 // plays the specified sound from the sounds map; assumes sound name is valid
 const playSound = (name: string) => {
-    const sound: HTMLAudioElement = sounds.get(name) as HTMLAudioElement;
-    sound.currentTime = 0;
+    const sound: Howl = sounds.get(name) || new Howl({src: ''});
+    sound.stop();
     sound.play();
 }
 
