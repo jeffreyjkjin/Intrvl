df34e54cb93e0d580209bde3d9db3df5df131272
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-06-27 01:01:15 -0700

    Updated sounds map
    -Installed Howler package
    -Replaced HTMLAudioElement with Howl

--- src/utilities/sounds.ts
+++ src/utilities/sounds.ts
@@ -1,20 +1,22 @@
+import { Howl } from 'howler'
+
 // map of sound names and their corresponding audio elements
-export const sounds = new Map<string, HTMLAudioElement>([
-    ['Alarm', new Audio('/sounds/alarm.wav')],
-    ['Beep', new Audio('/sounds/beep.wav')],
-    ['Bell', new Audio('/sounds/bell.wav')],
-    ['Bicycle Bell', new Audio('/sounds/bicycle-bell.wav')],
-    ['Buzzer', new Audio('/sounds/buzzer.wav')],
-    ['Cuckoo', new Audio('/sounds/cuckoo.wav')],
-    ['Ding', new Audio('/sounds/ding.wav')],
-    ['Double Ding', new Audio('/sounds/ding-ding.wav')],
-    ['Mario Coin', new Audio('/sounds/mario-coin.wav')],
-    ['Ring Tone', new Audio('/sounds/ringtone.wav')],
-    ['School Bell', new Audio('/sounds/school-bell.wav')],
-    ['Sonic Ring', new Audio('/sounds/sonic-ring.wav')],
-    ['Tap', new Audio('/sounds/tap.wav')],
-    ['Triple Bell', new Audio('/sounds/triple-bell.wav')],
-    ['Triple Tap', new Audio('/sounds/triple-tap.wav')],
-    ['Whistle', new Audio('/sounds/whistle.wav')],
-    ['None', new Audio()]
+export const sounds = new Map<string, Howl>([
+    ['Alarm', new Howl({src: ['/sounds/alarm.wav']})],
+    ['Beep', new Howl({src: ['/sounds/beep.wav']})],
+    ['Bell', new Howl({src: ['/sounds/bell.wav']})],
+    ['Bicycle Bell', new Howl({src: ['/sounds/bicycle-bell.wav']})],
+    ['Buzzer', new Howl({src: ['/sounds/buzzer.wav']})],
+    ['Cuckoo', new Howl({src: ['/sounds/cuckoo.wav']})],
+    ['Ding', new Howl({src: ['/sounds/ding.wav']})],
+    ['Double Ding', new Howl({src: ['/sounds/ding-ding.wav']})],
+    ['Mario Coin', new Howl({src: ['/sounds/mario-coin.wav']})],
+    ['Ring Tone', new Howl({src: ['/sounds/ringtone.wav']})],
+    ['School Bell', new Howl({src: ['/sounds/school-bell.wav']})],
+    ['Sonic Ring', new Howl({src: ['/sounds/sonic-ring.wav']})],
+    ['Tap', new Howl({src: ['/sounds/tap.wav']})],
+    ['Triple Bell', new Howl({src: ['/sounds/triple-bell.wav']})],
+    ['Triple Tap', new Howl({src: ['/sounds/triple-tap.wav']})],
+    ['Whistle', new Howl({src: ['/sounds/whistle.wav']})],
+    ['None', new Howl({src: ['']})]
 ]);
